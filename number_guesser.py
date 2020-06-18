96b93feceb4eca145879e2c1ad827964391a8847
rascherb95 <67018023+rascherb95@users.noreply.github.com>
2020-06-18 11:03:42 -0400

    Update number_guesser.py
    see lines 76-79, line 120
    
    game works but sloppy code

--- number_guesser.py
+++ number_guesser.py
@@ -1,12 +1,14 @@
 import random
 import time
+import sys
 
 start_game = input('''\nWould you like to play "Number Guesser?"\n\t\ty/n >\t''')
 
 if start_game == 'y':
     print('Good choice!')       #allow user specify play or nah
 else:
-    quit()
+    print('See you later!')
+    sys.exit()
 
 print('Welcome to the Number Guesser!')
 
@@ -16,7 +18,22 @@ print('Select two numbers')
 
 time.sleep(1)
 
-num1, num2 = int(input('#1: ')),int(input('#2: ')) #integer checks
+while True:
+    try:
+        num1 = int(input('Number #1 > \t'))
+        break
+    except:
+        ValueError
+        print('Please type a number')
+
+while True:
+    try:
+        num2 = int(input('Number #2 > \t'))
+        break
+    except:
+        ValueError
+        print('Please type a number')
+
 
 time.sleep(1)
 
@@ -31,31 +48,93 @@ time.sleep(1)
 
 print('How many guesses would you like?')
 
-lives = int(input('Select a number of lives:\n\t'))
+while True:
+    try:
+        lives = int(input('> \t'))
+        break
+    except:
+        ValueError
+        print('Please type a number')
 
 time.sleep(1)
 
-print("You'll start with", lives, 'lives')
+if lives > 0:
+    
+    print("You'll start with", lives, 'lives')
 
-guesses_taken = 0
+else:
 
-while guesses_taken < lives:
-    print('What do you think the answer is?')
-    guess = int(input()) #add integer checks
-    if guess == correct_answer:
-        print('Congrats! You won!')
-        quit()
-    #elif:
-    #    pass
+    print("So you dont want to play")
+
+time.sleep(1)
+
+    print('See you later!')
+
+time.sleep(1)
+
+
+#line 68 is only there to set a lives_left val, as setting it = 0 breaks code
+#playerwin is changed by the nested ifs in the while loop
+#guessses+taken isnt
+#workaround is using lives_left -=1 instead of guesses_taken += 1
+
+
+guesses_taken = 0    #changed all g_t code to lives_left after g_t didnt work
+lives_left = lives - guesses_taken   #doesn't run without vars. cant set = 0
+playerwin = 'no' #this variable is changed by the guess == correct lines
+
+
+
+while lives_left > 0 :
+    if playerwin == 'no':
+
+        if lives_left == 1: #correctly runs
+            print('Last life! Make it count')
+            while True:
+                try:
+                    guess = int(input('> \t'))
+                    if guess == correct_answer:
+                        playerwin = 'yessir'  #to check which win condition
+                    else:
+                        print('Wrong 1!')
+                        lives_left -= 1
+                    break
+                except:
+                    ValueError
+                    print('Please guess a number 1')
+
+        else: #correctly runs
+            time.sleep(.5)
+            print(f'You have {lives_left} lives remaining') #correctly prints
+            print('What\'s my number?')
+            while True:
+
+                try:
+                    guess = int(input('> \t'))
+                    if guess == correct_answer:
+                        playerwin = 'yes'
+
+                    else:
+                        print('Wrong!')
+                        lives_left -= 1    #this operates correctly
+                        guesses_taken += 1 #this doesnt do shit
+
+                    break
+
+                except:
+                    ValueError
+                    print('Please type a number')
+
+
+        if lives_left == 0: #correctly runs except for starting with 0 lives
+            print('Sorry, you ran out of lives!')
+            sys.exit()
+
+    elif playerwin == 'yessir':   #win 1 & win2 conditions properly work
+        print('Win 1')
+        sys.exit()
     else:
-        guesses_taken += 1
-        print('Ha! Try again')
-        time.sleep(1)
-
-if guesses_taken == lives:
-    print('Sorry, you ran out of lives!')
-    quit()
-
-
+        print('win 2')
+        sys.exit()
 
     #quit()
