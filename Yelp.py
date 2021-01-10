d41a74053a065f4dd0660bfcba6bf29e502875cb
rob <rascherb95@gmai.com>
2021-01-10 14:46:28 -0500

    to add to github

---
+++ Yelp.py
@@ -0,0 +1,37 @@
+#Yelp API
+
+#Business Search  url -- 'https:/api.yelp.com/v3/businesses/search'
+#Business Match   url -- 'https:/api.yelp.com/v3/businesses/match'
+#Business Phone   url -- 'https:/api.yelp.com/v3/businesses/search/phone/
+
+#Business Details url -- 'https://api.yelp.com/v3/businesses/{id}'
+#Business Reviews url -- 'https://api.yelp.com/v3/businesses/{id}/reveiws'
+
+# api --  nryKeiIA_hhJyilGu5gem6jFKJHy-UvSLaTTY-aHIpWLwRHJMKEjC9qKUfRND6AHP3J6Bc_M___pIy42YSPApMLl0zv_-ZNIVI64mKvQ33qY3-02wY_56SlZHlL7X3Yx
+
+# Import the modules
+import requests
+import JSON
+
+# Define a business ID
+#business_id =
+
+# define the API key, Define the endpoint, and then define the reader
+API_Key = 'nryKeiIA_hhJyilGu5gem6jFKJHy-UvSLaTTY-aHIpWLwRHJMKEjC9qKUfRND6AHP3J6Bc_M___pIy42YSPApMLl0zv_-ZNIVI64mKvQ33qY3-02wY_56SlZHlL7X3Yx'
+ENDPOINT = 'https:/api.yelp.com/v3/businesses/search'
+HEADERS = {'Authorization': 'bearer %s' % API_Key}
+
+# define the parameters. key is parameter,
+PARAMETERS = {'term':'pizza',
+              'limit': 50,
+              'radius': 10000,
+              'location': 'Philadelphia'}
+
+# make a request to the yelp API_Key
+#create a variable to store response
+Response = requests.get(URL = ENDPOINT, params = PARAMETERS, headers = HEADERS)
+
+#convert JSON to a dictionary that we can iterate through
+business_data = response.json()
+
+print(business_data)
