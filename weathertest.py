46dbd79681a4c78c72670b752a2f61813cef123d
rob <rascherb95@gmail.com>
2021-01-14 19:36:03 -0500

    initial weather commit

--- weathertest.py
+++ weathertest.py
@@ -1,33 +1,41 @@
-#weather
-
 import requests
 import json
+import ssl
+import smtplib
 
-api_key = '695b86f565c2d9468123e937ad407980'
-city = input(print("What City? >>"))
-state = input(print("What State? >>"))
+api_key = ''
+city = 'Boothwyn'
+state = 'Philadelphia'
 
-#url
+
 url_id = "http://api.openweathermap.org/data/2.5/weather?q={},{}&units=imperial&appid={}".format(city,state,api_key)
 
 response = requests.get(url_id)
 weather_data = json.loads(response.text)
 
-if response:
-    pass
-else:
-    print('Re-enter your location')
-    exit()
-
 air_temp = weather_data['main']['temp']
 real_feel = weather_data['main']['feels_like']
 delta = air_temp - real_feel
 
+#email variables
+sender_email = 'rascherbpythontesting@gmail.com'
+receiver_email = 'gabbyt321@gmail.com'
 
-print('The air temp is  {0}'.format(air_temp))
-print('The real feel is {0}'.format(real_feel))
+message = """\
+Subject: Your weather report for {}, {}
 
-if air_temp > real_feel:
-    print("It's colder than it feels")
-else:
-    print ("It's warmer than you think")
+It is currently  {} degrees outside
+The real feel is {} degrees
+
+Send via python""".format(city,state,air_temp,real_feel)
+
+#encrpytion
+port = 465
+password = input("Password >>>")
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', port,
+                        context=context) as server:
+    server.login('rascherbpythontesting@gmail.com',password)
+    server.sendmail(sender_email,receiver_email,message)
