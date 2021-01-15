00c57ebecf08d18796560c66878b9b4962fe7c0b
rob <rascherb95@gmail.com>
2021-01-14 19:36:03 -0500

    asdf

--- weatherlist.py
+++ weatherlist.py
@@ -1,21 +1,46 @@
-#automatedweatherlist
-
+import requests
+import json
+import ssl
+import smtplib
+import datetime
 import csv
+from decouple import config
 
-# 1 import mailing_list from csv
-Col1 = "Email"
-Col2 = "City"
-Col3 = "State"
+api_key = config('weather_api')
+#password = config('mailer_pass')
 
-mailing_list = {Col1:[], Col2:[], Col3:[]}
-csvfile = csv.reader(open('mailing_list.csv','r'))
-for row in csvfile:
-  mailing_list[Col1].append(row(0))
-  mailing_list[Col1].append(row(1))
-  mailing_list[Col1].append(row(2))
+#global url inherited by each object
+#note - to add functionality for more parameters. currently only city, state, apikey & defaulted fahrenheit
+url_id = "http://api.openweathermap.org/data/2.5/weather?q={},{}&units=imperial&appid={}"
 
 
-# 2 assign mailing_list data to email, city and state vars
-# 3 scheduler to mail everyday at noon
-# 4 get api data using cit and state vars
-# 5 mail to each person in mailing list
+#define our object
+class ListRecipient:
+    def __init__(self,email,city,state):
+        self.email = email
+        self.city = city
+        self.state = state
+        self.api_url = url_id.format(self.city,self.state,api_key)
+
+    def get_weather_data(self):
+        response = requests.get(self.api_url)
+        weather_data = json.loads(response.text)
+        print(weather_data)  #this prints all JSON data
+
+    def get_current_temp(self):
+        current_temp = weather_data['main']['temp']
+    def get_real_feel(self):
+        real_feel = weather_data['main']['temp']
+
+#create our list of ListRecipients
+mailing_list = []
+
+#to convert CSV data into ListReceipient objects, then added to MailingList
+with open('mailinglist.csv','r') as read_csv:
+    reader = csv.reader(read_csv,delimiter=',')
+    for row in reader:
+        mailing_list.append(ListRecipient(*row))
+
+for Recipient in mailing_list:
+    print(Recipient.get_current_temp)
+    print(Recipient.get_current_temp())
